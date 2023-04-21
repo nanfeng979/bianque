327ddc57c263e94e231be7d098aed6ae79504486
nanfeng979 <306338401@qq.com>
2023-04-21 13:34:00 +0800

    将时间的文本用精灵代替

--- Scripts/UI/worldTime/WorldDayManager.cs
+++ Scripts/UI/worldTime/WorldDayManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class WorldDayManager : MonoBehaviour
 {
-    public TMP_Text worldDayText;
+    // public TMP_Text worldDayText;
+    [SerializeField] private Image firstNumber;
+    [SerializeField] private Image secondNumber;
+
+    [SerializeField] private List<Sprite> sprites;
 
     private int currentWorldDay = 35;
 
@@ -17,7 +21,12 @@ public class WorldDayManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        worldDayText.text = currentWorldDay.ToString();
+        // worldDayText.text = currentWorldDay.ToString();
+
+        int firstNum = currentWorldDay / 10;
+        int secondNum = currentWorldDay % 10;
+        firstNumber.sprite = sprites[firstNum];
+        secondNumber.sprite = sprites[secondNum];
     }
 
     public void addWorldDay() {
