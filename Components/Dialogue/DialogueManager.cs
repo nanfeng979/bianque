ea3cdac97a46c4c78c1f48fee0f62976d80201cd
nanfeng979 <306338401@qq.com>
2023-05-29 13:44:03 +0800

    增加开始情节

--- Components/Dialogue/DialogueManager.cs
+++ Components/Dialogue/DialogueManager.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour
 {
     public DialogueDatabase database;
     public TMP_Text _name;
     public TMP_Text context;
+    public Text text;
+
+    public int dialogueIndex = 0;
 
     void Start()
     {
-        _name.text = database.Dialogue[0].characterName;
-        context.text = database.Dialogue[0].characterDialogueContent;
+        updateDialogueData(dialogueIndex);
     }
 
     void Update()
     {
         
+    }
+
+    private void updateDialogueData(int index) {
+        _name.text = database.Dialogue[index].characterName;
+        context.text = database.Dialogue[index].characterDialogueContent;
+        text.text = database.Dialogue[index].characterDialogueContent;
+    }
+
+    public void OnClick() {
+        dialogueIndex += 1;
+        if(dialogueIndex >= database.Dialogue.Length) {
+            dialogueIndex = 0;
+            nextScene();
+            return;
+        }
+
+        updateDialogueData(dialogueIndex);
+    }
+
+    private void nextScene() {
+        SceneManager.LoadScene("Playing");
     }
 }
